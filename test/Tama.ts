8b0f4fe9e8288d93f8960dee5d7f678334da2ff9
cylez <54079958+cyclez@users.noreply.github.com>
2024-02-18 03:24:50 +0100

    tests working

--- test/Tama.ts
+++ test/Tama.ts
@@ -7,21 +7,24 @@ describe("Tama", () => {
     async function deployContractFixture() {
 
         const tama = await hre.viem.deployContract("contracts/Tama.sol:Tama");
-        /*     const character0 = await hre.viem.deployContract("Character0");
-            const character1 = await hre.viem.deployContract("Character1");
-            const character2 = await hre.viem.deployContract("Character2");
-            const character3 = await hre.viem.deployContract("Character3"); */
 
-        return { tama };
-    }
-    it("Should fill contracts", async function () {
-        const { tama } = await loadFixture(deployContractFixture);
         const character0 = await hre.viem.deployContract("Character0");
         const character1 = await hre.viem.deployContract("Character1");
         const character2 = await hre.viem.deployContract("Character2");
         const character3 = await hre.viem.deployContract("Character3");
 
         await tama.write.setCharacters([character0.address, character1.address, character2.address, character3.address]);
+
+        const value = parseEther("0.01");
+        const one = 1;
+        await tama.write.purchase([1n], { value });
+        await tama.write.start([0n]);
+
+        return { tama };
+    }
+    it("Should fill contracts", async function () {
+        const { tama } = await loadFixture(deployContractFixture);
+
         const char0 = await tama.read.tama0();
         const char1 = await tama.read.tama1();
         const char2 = await tama.read.tama2();
@@ -32,17 +35,27 @@ describe("Tama", () => {
 
     it("Should mint", async function () {
         const { tama } = await loadFixture(deployContractFixture);
-        const value = parseEther("0.01");
-        await tama.write.purchase([1n], { value });
-        expect(tama.read.totalSupply).equal([1n]);
+        expect(await tama.read.totalSupply()).to.equal(1n);
     })
 
     it("Should play", async function () {
         const { tama } = await loadFixture(deployContractFixture);
-        const value = parseEther("0.01");
-        await tama.write.purchase([1n], { value });
         await tama.write.play([0n]);
-        const counter = await tama.read.gameData([0n]).counter;
-        expect(counter).equal([10n]);
+        const getData = await tama.read.gameData([0n]);
+        const counter = getData[4];
+        expect(counter).equal(10n);
+    })
+
+    it("Should eat", async function () {
+        const { tama } = await loadFixture(deployContractFixture);
+        const tamaFood = await hre.viem.deployContract("contracts/Tama.sol:TamaFood");
+        await tama.write.setTamaFoodAddress([tamaFood.address]);
+        const valueToken = parseEther("1");
+        await tamaFood.write.mint([`0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266`], { value: valueToken });
+        await tamaFood.write.approve([`0x5fbdb2315678afecb367f032d93f642f64180aa3`, 500000000000000000000n]);
+        await tama.write.eat([0n]);
+        const getData = await tama.read.gameData([0n]);
+        const lastEat = getData[2];
+        expect(lastEat).not.equal(0n);
     })
 })
